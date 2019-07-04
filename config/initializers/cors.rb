df8d8219592a87c3e889adf6e81f8e38cd98a095
Divepit <thedivepit@gmail.com>
2019-07-04 23:20:40 +0200

    fix in cors config and gitignore

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:8080', 'http://napoly.ch']
+    origins ['http://localhost:8080', 'https://napoly.ch']
 
     resource '*',
       headers: :any,
